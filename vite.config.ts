b88c0138bfb49012a4ea414349edf989d7ab6688
vpuri <vpuri22@uwo.ca>
2025-01-16 23:42:59 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,14 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 const conditionalPlugins: [string, Record<string, any>][] = [];
 
+// @ts-ignore
 if (process.env.TEMPO === "true") {
   conditionalPlugins.push(["tempo-devtools/swc", {}]);
 }
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",  // Ensure this matches the subdirectory for GitHub Pages
-  build: {
-    // Ensure all assets and chunk files are properly output with hashed names
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name]-[hash][extname]",  // Ensures hashed names for assets
-        chunkFileNames: "assets/[name]-[hash].js",  // For JS chunks as well
-        entryFileNames: "assets/[name]-[hash].js",  // For entry points
-      },
-    },
-  },
+  base: "/Portfolio",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
